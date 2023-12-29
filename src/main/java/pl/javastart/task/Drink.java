8d8401afe42939d588ba5f2f7945a4c947c9445c
Hanna Wojtala <hanna.e.wojtala@gmail.com>
2023-12-29 19:26:36 +0100

    zadanie_2_3 Drink do sprawdzenia

--- src/main/java/pl/javastart/task/Drink.java
+++ src/main/java/pl/javastart/task/Drink.java
@@ -4,5 +4,7 @@ public class Drink {
     String name;
     double price;
     boolean alcoholic;
-    Ingredient ingredient;
+    Ingredient ingredient1;
+    Ingredient ingredient2;
+    Ingredient ingredient3;
 }
--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -3,25 +3,29 @@ package pl.javastart.task;
 public class Main {
 
     public static void main(String[] args) {
+        Ingredient water = new Ingredient();
+        water.name = "Woda gazowana";
+        water.quantity = 200;
+        Ingredient syrup = new Ingredient();
+        syrup.name = "Syrop Mojito Mint Monin";
+        syrup.quantity = 20;
+        Ingredient limeJuice = new Ingredient();
+        limeJuice.name = "Sok z limonki";
+        limeJuice.quantity = 20;
+
         Drink mojito = new Drink();
         mojito.name = "Mojito bezalkoholowe";
         mojito.price = 38.90;
         mojito.alcoholic = false;
-        Ingredient ingredient1 = new Ingredient();
-        ingredient1.name = "Woda gazowana";
-        ingredient1.quantity = 200;
-        Ingredient ingredient2 = new Ingredient();
-        ingredient2.name = "Syropu Mojito Mint Monin";
-        ingredient2.quantity = 20;
-        Ingredient ingredient3 = new Ingredient();
-        ingredient3.name = "Sok z limonki";
-        ingredient3.quantity = 20;
+        mojito.ingredient1 = water;
+        mojito.ingredient2 = syrup;
+        mojito.ingredient3 = limeJuice;
 
         System.out.println("Drink: " + mojito.name);
         System.out.println("Cena: " + mojito.price + " zł");
         System.out.println("Czy zawiera alkohol? " + mojito.alcoholic);
-        System.out.println("Składniki: \n" + ingredient1.name + "\n" + ingredient2.name + "\n" + ingredient3.name);
-        System.out.println("Pojemność: " + (ingredient1.quantity + ingredient2.quantity + ingredient3.quantity));
+        System.out.println("Składniki: \n" + mojito.ingredient1.name + "\n" + mojito.ingredient2.name + "\n" + mojito.ingredient3.name);
+        System.out.println("Pojemność: " + (mojito.ingredient1.quantity + mojito.ingredient2.quantity + mojito.ingredient3.quantity));
 
     }
 }
